6f2b284f2baefbd3abbbfb826f773b24dae1cf05
James Turk <james.p.turk@gmail.com>
2005-02-04 08:12:14 +0000

    shared_ptr

--- include/types.h
+++ include/types.h
@@ -5,10 +5,13 @@
 //  James Turk (jpt2433@rit.edu)
 //
 // Version:
-//  $Id: types.h,v 1.1 2005/01/27 03:35:23 cozman Exp $
+//  $Id: types.h,v 1.2 2005/02/04 08:12:14 cozman Exp $
 //
 // Revisions:
 //  $Log: types.h,v $
+//  Revision 1.2  2005/02/04 08:12:14  cozman
+//  shared_ptr
+//
 //  Revision 1.1  2005/01/27 03:35:23  cozman
 //  initial import (exceptions,types, and logging,oh my!)
 //
@@ -17,23 +20,29 @@
 #ifndef PHOTON_TYPES_H
 #define PHOTON_TYPES_H
 
+#include <boost/shared_ptr.hpp>
+
 // Title: Basic Types
 
 namespace photon {
 
 // Group: Types
 
-    // Typedef: ubyte
+    // Type: ubyte
     //  Unsigned byte, alias for unsigned char.
     typedef unsigned char ubyte;
 
-    // Typedef: uint
+    // Type: uint
     //  Alias for unsigned integer.
     typedef unsigned int uint;
 
-    // Typedef: scalar
+    // Type: scalar
     //  Scalar value, used throughout photon. (double or float)
     typedef double scalar;
+    
+    // Type: shared_ptr
+    //  Shared pointer type. (uses the boost implementation)
+    using boost::shared_ptr;
 
 }
 
