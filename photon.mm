fb8d39e7676f45cddb499eac41bccd1b5831ea63
James Turk <james.p.turk@gmail.com>
2005-05-15 03:00:07 +0000

    MindMap (http://freemind.sourceforge.net/wiki/) file

---
+++ photon.mm
@@ -0,0 +1,80 @@
+<map version="0.8.0 RC2">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node ID="Freemind_Link_1676542623" TEXT="Photon">
+<cloud/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node ID="_" POSITION="right" TEXT="0.1 Release">
+<arrowlink DESTINATION="Freemind_Link_511487087" ENDARROW="Default" ENDINCLINATION="117;-117;" ID="Freemind_Arrow_Link_1969744902" STARTARROW="None" STARTINCLINATION="-97;-427;"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node ID="Freemind_Link_1613164220" TEXT="better SConstruct file">
+<icon BUILTIN="button_ok"/>
+</node>
+<node ID="Freemind_Link_614612335" TEXT="all stable ZEngine/Nova features">
+<font NAME="SansSerif" SIZE="12"/>
+<node ID="Freemind_Link_107267630" TEXT="&quot;Engine&quot;"/>
+<node FOLDED="true" ID="Freemind_Link_50716011" TEXT="Texture">
+<node ID="Freemind_Link_561271163" TEXT="Font"/>
+<node ID="Freemind_Link_385334177" TEXT="Image"/>
+</node>
+<node ID="Freemind_Link_1851655735" TEXT="Music"/>
+<node ID="Freemind_Link_1045379727" TEXT="Sound"/>
+<node ID="Freemind_Link_1795651487" TEXT="ResourceManagers"/>
+</node>
+<node ID="Freemind_Link_188779968" TEXT="Test Suite">
+<node ID="Freemind_Link_1570884553" TEXT="port test-suite to Saturn core">
+<icon BUILTIN="button_ok"/>
+</node>
+<node ID="Freemind_Link_1104675603" TEXT="Tests">
+<node ID="Freemind_Link_1283736172" TEXT="Math Test(s)"/>
+<node ID="Freemind_Link_1348104879" TEXT="Kernel/AppCore"/>
+</node>
+<node ID="Freemind_Link_1937076235" TEXT="Examples">
+<node ID="Freemind_Link_279976301" TEXT="Pen"/>
+<node ID="Freemind_Link_25725154" TEXT="Sound/Music"/>
+<node ID="Freemind_Link_288973656" TEXT="Texture"/>
+</node>
+</node>
+<node FOLDED="true" ID="Freemind_Link_22783417" TEXT="Example Game">
+<node ID="Freemind_Link_714736465" TEXT="Drawing Program?"/>
+</node>
+</node>
+<node FOLDED="true" ID="Freemind_Link_486829238" POSITION="right" TEXT="0.2 Release">
+<node ID="Freemind_Link_216021234" TEXT="Sprite System">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node ID="Freemind_Link_1497432478" TEXT="Particle System"/>
+<node ID="Freemind_Link_654490435" TEXT="Second Example Game"/>
+</node>
+<node FOLDED="true" ID="Freemind_Link_1046282376" POSITION="left" TEXT="What is the Saturn Core?">
+<font NAME="SansSerif" SIZE="12"/>
+<node ID="Freemind_Link_1988477058" TEXT="New Core System"/>
+<node ID="Freemind_Link_375964485" TEXT="Core/Task/Kernel design"/>
+<node ID="Freemind_Link_1501464831" TEXT="Tasks are &quot;rings&quot; in that they are fundamental to &#xa;the Cores which they are associated with."/>
+<node FOLDED="true" ID="Freemind_Link_953342417" TEXT="Also the 6th design from ZEngine">
+<node ID="Freemind_Link_1672529041" TEXT="Mercury - Original ZEngine"/>
+<node ID="Freemind_Link_1954983922" TEXT="Venus - Second ZEngine (0.7/0.8)"/>
+<node ID="Freemind_Link_1420862156" TEXT="Earth - Nova"/>
+<node ID="Freemind_Link_1279393892" TEXT="Mars - Photon/Amph"/>
+<node ID="Freemind_Link_1083205841" TEXT="Jupiter - Initial rewrite of Photon"/>
+</node>
+</node>
+<node ID="Freemind_Link_511487087" POSITION="left" TEXT="Website">
+<node ID="Freemind_Link_1605443386" TEXT="cozTheme">
+<icon BUILTIN="help"/>
+</node>
+<node ID="Freemind_Link_1050052437" TEXT="phpWebSite">
+<icon BUILTIN="help"/>
+</node>
+<node ID="Freemind_Link_1940448315" TEXT="Wiki">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node ID="Freemind_Link_351891371" POSITION="left" TEXT="General Mateinance">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node ID="Freemind_Link_1978849189" TEXT="Clean up CVS">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node ID="Freemind_Link_1642641448" TEXT="Ensure compilation succeeds on Win/Linux simultaneously"/>
+</node>
+</node>
+</map>
