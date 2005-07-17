925a96ca0cfc8985151e32f54b53566d80533c4b
James Turk <james.p.turk@gmail.com>
2005-07-17 22:41:03 +0000

    added documentation text files

---
+++ README.txt
@@ -0,0 +1,30 @@
+Thank you for downloading photon,
+    photon is an API which is designed primarily to ease creation of hardware 
+accelerated 2D games with OpenGL and other open source & cross platform 
+libraries. Photon aims to simplify portable game development, enabling 
+developers to focus on the gameplay. Photon uses OpenGL for hardware accelerated
+rotation, alpha blending, resizing, and drawing. Photon is built entirely on 
+free software, using libraries like GLFW, PhysFS, OpenAL, and boost. Using 
+Photon does not require knowledge of any of these libraries as their needed 
+functionality is wrapped entirely within Photon’s rich class library so that 
+anyone with a decent grasp of C++ can use Photon.
+    photon is a cross platform API, designed primarily for game development.
+photon is designed to run on as many operating systems as possible, and OS 
+support is only limited by libraries which Photon depends on, at the moment
+photon should run on Windows, Linux, OSX, and other Operating Systems
+which have OpenGL support. 
+
+-------
+
+If this is a source distribution you'll need to build photon before you can use 
+it in your own applications, for details on how to build and install photon 
+check out INSTALL.txt.  
+
+If this is a binary distribution you can start using photon right away by 
+linking the photon library (found in the lib/ subfolder) and it's dependencies
+with your applications.
+
+If you need any help or have any questions check out IFAQ.txt or visit photon's
+homepage at http://photon.sourceforge.net/.
+
+$Id: README.txt,v 1.1 2005/07/17 22:41:03 cozman Exp $
--- photon.mm
+++ photon.mm
@@ -3,59 +3,31 @@
 <node ID="Freemind_Link_1676542623" TEXT="Photon">
 <cloud/>
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node ID="_" POSITION="right" TEXT="0.1 Release">
-<font NAME="SansSerif" SIZE="12"/>
-<node ID="Freemind_Link_1613164220" TEXT="better SConstruct file">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_614612335" TEXT="all stable ZEngine/Nova features ">
-<font NAME="SansSerif" SIZE="12"/>
-<icon BUILTIN="button_ok"/>
-<node ID="Freemind_Link_642651361" TEXT="Colored fonts">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_985678305" TEXT="callbacks for better input handling"/>
-<node ID="Freemind_Link_485484686" TEXT="Music">
-<icon BUILTIN="help"/>
+<node ID="Freemind_Link_682620075" POSITION="right" TEXT="0.1 Release">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node ID="Freemind_Link_1479941799" TEXT="Clean up source">
+<node ID="Freemind_Link_1716705807" TEXT="clean up naturaldocs"/>
+<node ID="Freemind_Link_1080393911" TEXT="audio:: could using some cleaning">
+<node ID="Freemind_Link_632977685" TEXT="make compilation succeed without OpenAL"/>
 </node>
 </node>
-<node ID="Freemind_Link_1104675603" TEXT="Tests">
-<node ID="Freemind_Link_288973656" TEXT="Texture">
-<icon BUILTIN="button_ok"/>
+<node ID="Freemind_Link_1153941464" TEXT="test compilation on fresh systems">
+<node ID="Freemind_Link_1403005191" TEXT="test compilation on clean linux system"/>
+<node ID="Freemind_Link_617919930" TEXT="test compilation on windows system"/>
+<node ID="Freemind_Link_510637" TEXT="OSX tester?"/>
 </node>
-<node ID="Freemind_Link_1283736172" TEXT="Math Test">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_279976301" TEXT="Pen">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_609153459" TEXT="Image">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_1961072093" TEXT="Font">
-<icon BUILTIN="button_ok"/>
-</node>
-<node COLOR="#000000" ID="Freemind_Link_964892891" TEXT="Input">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_25725154" TEXT="Audio">
-<icon BUILTIN="button_ok"/>
-</node>
-<node ID="Freemind_Link_1348104879" TEXT="Kernel/AppCore">
-<node ID="Freemind_Link_1664207862" TEXT="how to test?"/>
-</node>
-</node>
-<node ID="Freemind_Link_22783417" TEXT="Example Game">
+<node ID="Freemind_Link_1010220321" TEXT="test all tests"/>
+<node ID="Freemind_Link_1870184326" TEXT="example game">
 <node ID="Freemind_Link_714736465" TEXT="Let&apos;s Draw"/>
 <node COLOR="#000000" ID="Freemind_Link_597173063" TEXT="Asteroids"/>
 </node>
-<node ID="Freemind_Link_743325646" TEXT="distribution documentation">
-<node ID="Freemind_Link_730720170" TEXT="credits"/>
-<node ID="Freemind_Link_564233418" TEXT="FAQ"/>
-<node ID="Freemind_Link_363257674" TEXT="contact info"/>
+<node ID="Freemind_Link_1746871436" TEXT="possible features">
+<node ID="Freemind_Link_40954797" TEXT="Decide on callbacks (push to 0.1.x possibly)"/>
+<node ID="Freemind_Link_547285954" TEXT="Decide on Kernel/Appcore Test"/>
+<node ID="Freemind_Link_1825420810" TEXT="Music"/>
 </node>
 </node>
-<node ID="Freemind_Link_486829238" POSITION="right" TEXT="0.2 Release">
+<node FOLDED="true" ID="Freemind_Link_486829238" POSITION="right" TEXT="0.2 Release">
 <node ID="Freemind_Link_1298931281" TEXT="XML resource files"/>
 <node ID="Freemind_Link_216021234" TEXT="Sprite System">
 <font NAME="SansSerif" SIZE="12"/>
@@ -70,18 +42,7 @@
 <icon BUILTIN="help"/>
 </node>
 </node>
-<node ID="Freemind_Link_351891371" POSITION="left" TEXT="General Mateinance">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node ID="Freemind_Link_1642641448" TEXT="Ensure compilation succeeds on Win/Linux simultaneously"/>
-<node ID="Freemind_Link_330674889" TEXT="Find an OSX user to test"/>
-<node ID="Freemind_Link_1190200631" TEXT="Clean up source"/>
-</node>
-<node ID="Freemind_Link_682620075" POSITION="left" TEXT="Current Problems">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node ID="Freemind_Link_1080393911" TEXT="audio:: could using some cleaning"/>
-<node ID="Freemind_Link_632977685" TEXT="make compilation succeed without OpenAL"/>
-</node>
-<node COLOR="#147f1e" ID="Freemind_Link_438641521" POSITION="left" TEXT="Version: $Id: photon.mm,v 1.18 2005/07/17 07:14:09 cozman Exp $">
+<node COLOR="#147f1e" ID="Freemind_Link_438641521" POSITION="left" TEXT="Version: $Id: photon.mm,v 1.19 2005/07/17 22:41:03 cozman Exp $">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
